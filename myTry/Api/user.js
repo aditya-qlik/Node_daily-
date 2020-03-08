55f54848ab20235f8b76a129cea1ca065ae4f640
aditya-qlik <aditya.kumar@qliktag.com>
2020-03-08 15:34:34 +0530

    my starter files

--- myTry/Api/user.js
+++ myTry/Api/user.js
@@ -3,14 +3,28 @@ const mongoose = require('mongoose');
 const User = require('../DB/User');
 const route = express.Router();
 
-route.post ('/',async (req,res) => {
-    const {firstName, lastName} = req.body;
-    let user = {};
-    user.firstName = firstName;
-    user.lastName = lastName;
-    let userModel = new User(user);
-    await userModel.save();
-    res.json(userModel);
+/*
+* route.post ('/',async (req,res) => {
+*     const {firstName, lastName} = req.body;
+*     let user = {};
+*     user.firstName = firstName;
+*     user.lastName = lastName;
+*     let userModel = new User(user);
+*     await userModel.save();
+*     res.json(userModel);
+* });
+*/
+route.get('/', (req,res) => {
+    console.log('Using get..');
+    // res.send('I did it on my own!');
+    // const sendJSON = {
+    //     name: 'Aditya',
+    //     age: 24,
+    //     cool: true
+    //     };
+          
+    //     res.json(sendJSON);
+    res.render('hello');
 });
 
 module.exports = route ;
--- myTry/server.js
+++ myTry/server.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const connectDB = require('./DB/connection');
 const app = express();
-
-
+const bodyParser = require('body-parser');
+const path = require('path');
+const route = require('./Api/user');
+app.use('/', route);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('views', path.join(__dirname, 'views')); // this is the folder where we keep our pug files
+app.set('view engine', 'pug');
 connectDB();
 app.use(express.json({extended: false}));
 app.use('/api/userModel', require('./Api/user'));
 const port = process.env.Port || 7777;
 
 app.listen(port, () => console.log('Server started'));
+app.set(port, process.env.PORT || 7777);
